2b17b2b6a452ae05a76c9d7d0a1286feb1401003
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2022-11-08 15:18:51 +0900

    컴퓨터가 조건에 맞는 적당한 랜덤값을 가져오도록 하는 generateNum 함수 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -67,5 +68,13 @@ public class Application {
         strikeCount = 0;
         ballCount = 0;
         computer.clear();
+        generateNum();
+    }
+
+    public static void generateNum() {
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) computer.add(randomNumber);
+        }
     }
 }
